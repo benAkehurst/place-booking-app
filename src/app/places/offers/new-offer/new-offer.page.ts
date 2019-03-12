96b9bda333e1d9709566cabfea61d9bb1df3d7f6
benAkehurst <benakehurst@hotmail.com>
2019-03-12 17:48:29 +0000

    Added Reactive form to new offers

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-offer',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewOfferPage implements OnInit {
 
+  /**
+   * Defines an new form
+   */
+  public form: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    /**
+     * Build a basic blank form when the page is generated
+     */
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      description: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+      price: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.min(1)]
+      }),
+      dateFrom: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      dateTo: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+    });
+  }
+
+  /**
+   * Creates a new offer
+   */
+  public onCreateOffer() {
+    console.log(this.form);
   }
 
 }
