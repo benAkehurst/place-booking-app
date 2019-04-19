36a36cadce66d118dd076fbd2d2a3680af4ae395
benAkehurst <benakehurst@hotmail.com>
2019-04-19 12:16:20 +0100

    shoing picked image

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  googleMapsApiKey: 'AIzaSyA8JAGNux_I3OzzyrkM6GGEChYg0mO1swQ'
 };
 
 /*
