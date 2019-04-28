5a253c77b9f54897796ed93c952252c5c7c85923
benAkehurst <benakehurst@hotmail.com>
2019-04-28 12:00:56 +0100

    add user sign up method

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -58,12 +58,12 @@ export class AuthPage implements OnInit {
     }
     const email = form.value.email;
     const password = form.value.password;
-    console.log(email, password);
-
     if (this.isLogin) {
       // Send a request to login servers
     } else {
-      // Send a request to signup servers
+      this.authService.signUp(email, password).subscribe(resData => {
+        console.log(resData);
+      });
     }
   }
 }
